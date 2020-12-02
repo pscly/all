4196a5cf9d3cb541c0448b7267fd8aed0e94304b
Pscly <pscly@outlook.com>
2020-12-02 15:08:13 +0800

    '版本1完成'

--- 02run_funcs/start.py
+++ 02run_funcs/start.py
@@ -12,12 +12,13 @@ def fun_func(func_id, funcs_):
     如果返回的是True，那就代表执行成功了
     '''
     if func_id == 'q':
+        quit("退出")
         return False
     if func_id not in funcs_:
         print('你输入有误')
         return False
 
-    funcs_[in_1][0]()
+    funcs_[func_id][0]()
     return True
     
 
@@ -30,7 +31,7 @@ funcs = {
     '3': [func3, '方法3'],
     '4': [func4, '方法4'],
     # '5': [runs, '输入一堆，然后按顺序执行'], # 也许我该让他默认就这样?
-    '5': [exit, '退出'],
+    'q': [None, '退出'],
 }
 
 def dayin(stat=''):
@@ -48,8 +49,8 @@ if __name__ == "__main__":
         in_1_list = in_1.split(' ')
         if not len(in_1_list) == 1:
             for i in in_1_list:
-                if not fun_func(i, funcs):
-                    pass
+                fun_func(i, funcs)
+                    
 
         else :
             fun_func(in_1, funcs)
